ed48c83903a9245c6707296324efbb5a34d66810
FengZhaolin <kingsimba0511@gmail.com>
2022-03-17 18:04:34 +0800

    first version

---
+++ README.md
@@ -0,0 +1,23 @@
+# Autoxing 机器人测试手册
+
+本文档使用 [VuePress](https://vuepress.vuejs.org/) 编写。
+
+## 调试运行
+
+首次初始化，要安装 VuePress 等依赖库
+
+```shell
+yarn install
+```
+
+调试运行：运行以下命令，然后用浏览器访问 http://localhost:8080/
+
+```shell
+yarn run docs:dev
+```
+
+
+
+
+
+
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    title: 'Autoxing REST API Book',
+    description: "A complete guide to control Autoxing Tech's Robots with REST API",
+    base: "/",
+    markdown: {
+        extendMarkdown: md => {
+            md.set({ breaks: true })
+            md.use(require('markdown-it-imsize'))
+        }
+    },
+    themeConfig: {
+        nav: [
+            { text: '底盘JIRA', link: 'https://autoxing.atlassian.net/browse/RCSS' }
+        ],
+        // sidebar: [
+        //     '/overview/overview',
+        //     '/reference/reference'
+        // ],
+        // sidebar: {
+        //     '/overview/': ["overview"],
+        //     '/reference/': ["reference"]
+        // },
+        sidebar: [
+            {
+                title: '概览',   // required
+                collapsable: false, // optional, defaults to true
+                sidebarDepth: 0,    // optional, defaults to 1
+                children: [
+                    '/overview/getting_start',
+                    '/overview/api_principals',
+                    '/overview/first_move',
+                ]
+            },
+            {
+                title: '参考手册',
+                collapsable: false,
+                children: [
+                    '/reference/maps',
+                    '/reference/moves',
+                ],
+                sidebarDepth: 2
+            }
+        ],
+        lastUpdated: 'Last Updated'
+    }
+}
