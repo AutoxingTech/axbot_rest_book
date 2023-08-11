5a15b1eada89da6985418b9215c7cab97c9063bd
FengZhaolin <kingsimba0511@gmail.com>
2023-08-11 21:45:02 +0800

    add 2.6.0-rc0

--- format_release_notes.py
+++ format_release_notes.py
@@ -3,5 +3,23 @@ import re
 
 
 def main():
+    with open("docs/reference/releases.md", "r", encoding="utf8") as f:
+        lines = f.readlines()
 
-    pass
+    for i, line in enumerate(lines):
+        m = re.match(r"### ((New Feature|Bug|Task|Improvement).*)", line)
+        if m:
+            line = "- " + m.group(1)
+
+        m = re.match(r"(\[RCSS-\d+\])\(http.*\) (.*)\n", line)
+        if m:
+            line = "  - " + m.group(1) + " " + m.group(2)
+
+        lines[i] = line
+
+    with open("docs/reference/releases.md", "w", encoding="utf8") as f:
+        f.writelines(lines)
+
+
+if __name__ == "__main__":
+    main()
